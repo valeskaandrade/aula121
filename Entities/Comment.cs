878de33379cda404ce3c07d78a406adb88845606
“valeskaandrade” <valeska.andrade@gmail.com>
2020-02-12 20:17:56 -0300

    aula 121 - stringbuilder

---
+++ Entities/Comment.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Publicacao.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+        public Comment()
+        {
+        }
+     
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Publicacao.Entities;
+using System.Collections.Generic; // para usar o List
+namespace Publicacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //aula 121 - stringbuilder
+            Console.WriteLine("Quantos Posts: ");
+            int p = int.Parse(Console.ReadLine());
+
+            List<Post> posts = new List<Post>();
+
+            for (int i= 1; i <= p; i++)
+            {
+                Console.Write("Moment: ");
+                DateTime moment = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Title: ");
+                string title = Console.ReadLine();
+
+                Console.Write("Content: ");
+                string content = Console.ReadLine();
+
+                Console.Write("Likes: ");
+                int likes = int.Parse(Console.ReadLine());
+
+                Post post = new Post(moment,title,content,likes);
+                
+                Console.Write("Quantos Comentários: ");
+                int c = int.Parse(Console.ReadLine());
+                for (int j = 1; j <= c; j++)
+                {
+                    Console.Write($"Comentário {j}: ");
+                    
+                    Comment comment = new Comment(Console.ReadLine());
+                    post.AddComment(comment);
+                }
+                posts.Add(post);
+            }
+            foreach (Post po in posts)
+            {
+                Console.WriteLine(po.ToString()); 
+            }
+        }
+    }
+}
